f0fcc359ce4d3eafbbb900678008eca0f40d1d25
Domirando <maisiedev@gmail.com>
2025-06-08 18:03:55 +0500

    struct has been added

--- src/struct.rs
+++ src/struct.rs
@@ -1,26 +1,53 @@
-//struct - a data structure that allows you to group multiple fields together under one name.
+fn struct_f() {
+    //     tuple
+    let _rect = (200, 500);
 
-fn struct(){
-    let mut bank_account = BankAccount{
-        owner: "Alice".to_string(),
-        balance: 150.55,
+    // a struct (short for "structure") is a custom data type that allows you to group together related data into a single unit.
+    struct _Book {
+        title: String,
+        author: String,
+        pages: String,
+        available: bool,
+    }
+
+    struct User {
+        active: bool,
+        username: String,
+        email: String,
+        sign_in: u64,
+    }
+
+    let mut user1 = User {
+        active: true,
+        username: String::from("domirando"),
+        email: String::from("smth@gmail.com"),
+        sign_in: 1,
     };
-    bank_account.check_balance();
-    bank_account.withdraw(50.48);
-    bank_account.check_balance();
-}
-struct BankAccount {
-    owner: String,
-    balance: f64,
-}
 
-impl BankAccount{
-    fn withdraw(&mut self, amount: f64){
-        println!("Withdrawing {} from account owned by {}", amount, self.owner);
-        self.balance -= amount;
-    }
+//     user1.email = String::from("sss");
+    println!("{}", user1.email);
 
-    fn check_balance(&self) {
-        println!("Account owned by {} has a balance of {}", self.owner, self.balance);
+    fn build_user(email:String, username:String) -> User{
+        User {
+            active: true,
+            email,
+            username,
+            sign_in: 2,
+        }
     }
+//     let user2 = User {
+//         email: String::from("another@"),
+//         ..user1
+//     };
+    let user2 = build_user(String::from("another@"), String::from("userr"));
+    println!("{}", user2.sign_in);
+    println!("{}", user2.email);
+    println!("{}", user2.username);
+
+    // tuple structs
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let white = Color(255, 255, 255);
 }
